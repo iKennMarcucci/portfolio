03022c7810043083357336cd27307adb1e0f3d18
Kenn Marcucci <marcucci5783@gmail.com>
2024-11-12 08:51:26 -0500

    Fix: Inline footer fixed

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Footer() {
    return (
-      <footer className="text-gray-400 text-sm flex w-full pt-40 pb-10 mx-auto
-      lg:max-w-[1000px] max-lg:max-w-[900px] max-lg:flex-col max-lg:text-center">
+      <footer className="text-gray-400 text-sm w-full pt-40 pb-10 mx-auto
+      lg:max-w-[1000px] max-lg:max-w-[900px] max-lg:text-center">
          © {new Date().getFullYear()} Kenn Marcucci. Este portfolio se inspiró en enfoques de diseño encontrados en&nbsp;
          <Link href={"https://porfolio.dev"} target="_blank" className="hover:underline font-semibold">
             midudev
