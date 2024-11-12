8f8d858330f34dc99a8fa41167ba306cee17d6b0
Kenn Marcucci <marcucci5783@gmail.com>
2024-11-12 08:44:42 -0500

    Fix: Cellphone rendering improved

--- src/components/partials/AuroraBg.jsx
+++ src/components/partials/AuroraBg.jsx
@@ -1,5 +1,5 @@
 export default function AuroraBg() {
    return (
-      <div className="background absolute top-0 -left-0 w-full -z-50" />
+      <div className="background absolute top-0 -left-0 w-full -z-50 max-sm:hidden" />
    )
 }
