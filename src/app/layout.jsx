7ff6dba5dafb46745f29596b9a0fa27cd1268cee
Kenn Marcucci <marcucci5783@gmail.com>
2024-11-21 14:06:36 -0500

    Fix: layout is now running on the server side

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,59 +1,22 @@
-"use client"
-
-import AuroraBg from "@/components/partials/AuroraBg"
-import Navbar from "@/components/partials/Navbar"
-import Footer from "@/components/partials/Footer"
-import { useEffect, useState } from "react"
 import { Onest } from "next/font/google"
 import "./globals.css"
 
 const onest = Onest({
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	subsets: ["latin", "latin-ext"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin", "latin-ext"]
 })
 
+export const metadata = {
+  title: 'Kenn Marcucci - Portfolio',
+  description: 'This portfolio was developed by Kenn Marcucci',
+}
+
 export default function RootLayout({ children }) {
-	const [metadata, setMetadata] = useState(null)
-
-	useEffect(() => {
-		async function fetchMetadata() {
-			let lang = "es"
-			if (localStorage.getItem("languaje") !== null) lang = localStorage.getItem("languaje")
-			const res = await fetch(`/api/layout/${lang}`)
-			const data = await res.json()
-			setMetadata({
-				title: data.title,
-				description: data.description,
-				viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
-				themeColor: "#3498db"
-			})
-		}
-
-		fetchMetadata()
-	}, [localStorage])
-
-	return metadata && (
-		<html lang="es">
-			<head>
-				<link rel="icon" href="/favicon.svg" type="image/png" sizes="32x32" />
-				<meta name="theme-color" content={"#030712"} />
-				<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-				<title>{metadata.title}</title>
-				<meta name="description" content={metadata.description} />
-			</head>
-			<body className={`${onest.className} grid min-h-[100dvh] grid-rows-[auto_1fr_auto] antialiased`}>
-				<header className="flex justify-center items-center">
-					<Navbar />
-				</header>
-				<main className="container mx-auto">
-					<AuroraBg />
-					{children}
-				</main>
-				<footer className="flex justify-center items-center container mx-auto">
-					<Footer />
-				</footer>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="es">
+      <body className={`${onest.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
 }
